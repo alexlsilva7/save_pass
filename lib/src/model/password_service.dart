e7241170847204a4b5988ef0cb35d5fa80be131f
alexlsilva7 <alex.lsilva154@gmail.com>
2022-10-28 13:58:02 -0300

    add model password service

---
+++ lib/src/model/password_service.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class PasswordService {
+  PasswordService({
+    required this.serviceName,
+    required this.username,
+    required this.password,
+  });
+
+  String serviceName;
+  String username;
+  String password;
+}
