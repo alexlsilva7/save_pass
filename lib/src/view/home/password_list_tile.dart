1f70864943d77e699fc850c0c2c805f14fafdcef
alexlsilva7 <alex.lsilva154@gmail.com>
2022-11-05 22:50:28 -0300

    swipe delete password service

--- lib/src/view/home/password_list_tile.dart
+++ lib/src/view/home/password_list_tile.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swipe_action_cell/core/cell.dart';
+import 'package:save_pass/src/controller/sqlite_password_controller.dart';
 import 'package:save_pass/src/model/password_model.dart';
 import 'package:save_pass/ui/colors.dart';
 import 'package:save_pass/ui/text_styles.dart';
+import 'package:provider/provider.dart';
 
 class PasswordListTile extends StatefulWidget {
   const PasswordListTile({super.key, required this.passwordService});
@@ -29,58 +32,88 @@ class _PasswordListTileState extends State<PasswordListTile> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: isPasswordVisible ? null : AppColors.black800,
-            gradient: isPasswordVisible
-                ? LinearGradient(
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    colors: [
-                      AppColors.primary.withOpacity(0.2),
-                      AppColors.black800,
-                    ],
-                  )
-                : null,
-            borderRadius: const BorderRadius.all(
-              Radius.circular(4),
-            ),
-          ),
-          child: ListTile(
-            leading: IconButton(
-              icon: Icon(
-                isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                color:
-                    isPasswordVisible ? AppColors.primary : AppColors.gray500,
-              ),
-              onPressed: () {
-                setState(() {
-                  isPasswordVisible = !isPasswordVisible;
-                });
+        SwipeActionCell(
+          key: ValueKey(username + service),
+          closeWhenScrolling: true,
+          leadingActions: const [
+            // SwipeAction(
+            //   title: 'Edit',
+            //   color: Colors.amber,
+            //   onTap: (handler) async {
+            //     handler(false); // To close the cell
+            //   },
+            // ),
+          ],
+          trailingActions: <SwipeAction>[
+            SwipeAction(
+              title: 'Delete',
+              onTap: (CompletionHandler handler) async {
+                await handler(true);
+                context
+                    .read<SQlitePasswordController>()
+                    .deletePassword(widget.passwordService);
+                setState(() {});
               },
+              color: AppColors.red,
+              backgroundRadius: 5,
+              nestedAction:
+                  SwipeNestedAction(title: 'Confirm?', nestedWidth: 130),
             ),
-            title: AnimatedDefaultTextStyle(
-              style: AppTextStyle.bodyText1.copyWith(
-                color: isPasswordVisible ? AppColors.gray200 : AppColors.white,
-                fontSize: isPasswordVisible ? 14 : 16,
+          ],
+          child: Container(
+            decoration: BoxDecoration(
+              color: isPasswordVisible ? null : AppColors.black800,
+              gradient: isPasswordVisible
+                  ? LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [
+                        AppColors.primary.withOpacity(0.2),
+                        AppColors.black800,
+                      ],
+                    )
+                  : null,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(4),
               ),
-              duration: const Duration(milliseconds: 300),
-              child: Text(service),
             ),
-            subtitle: AnimatedDefaultTextStyle(
-              style: AppTextStyle.subtitle2.copyWith(
-                color:
-                    isPasswordVisible ? AppColors.primary : AppColors.gray500,
-                fontSize: isPasswordVisible ? 16 : 14,
+            child: ListTile(
+              leading: IconButton(
+                icon: Icon(
+                  isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                  color:
+                      isPasswordVisible ? AppColors.primary : AppColors.gray500,
+                ),
+                onPressed: () {
+                  setState(() {
+                    isPasswordVisible = !isPasswordVisible;
+                  });
+                },
               ),
-              duration: const Duration(milliseconds: 300),
-              child: Text(
-                isPasswordVisible ? password : username,
+              title: AnimatedDefaultTextStyle(
+                style: AppTextStyle.bodyText1.copyWith(
+                  color:
+                      isPasswordVisible ? AppColors.gray200 : AppColors.white,
+                  fontSize: isPasswordVisible ? 14 : 16,
+                ),
+                duration: const Duration(milliseconds: 300),
+                child: Text(service),
+              ),
+              subtitle: AnimatedDefaultTextStyle(
+                style: AppTextStyle.subtitle2.copyWith(
+                  color:
+                      isPasswordVisible ? AppColors.primary : AppColors.gray500,
+                  fontSize: isPasswordVisible ? 16 : 14,
+                ),
+                duration: const Duration(milliseconds: 300),
+                child: Text(
+                  isPasswordVisible ? password : username,
+                ),
               ),
             ),
           ),
         ),
-        const SizedBox(height: 8),
+        const SizedBox(height: 8)
       ],
     );
   }
